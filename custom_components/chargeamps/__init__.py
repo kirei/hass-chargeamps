a0d29d6195fb44ae71c785271b349b469f90d938
Jakob Schlyter <jakob@kirei.se>
2020-06-24 20:06:31 +0200

    reformat

--- custom_components/chargeamps/__init__.py
+++ custom_components/chargeamps/__init__.py
@@ -9,9 +9,13 @@ import logging
 from datetime import timedelta
 from typing import Optional
 
-import voluptuous as vol
-
 import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
+from homeassistant.const import CONF_API_KEY, CONF_PASSWORD, CONF_URL, CONF_USERNAME
+from homeassistant.helpers import discovery
+from homeassistant.helpers.entity import Entity
+from homeassistant.util import Throttle
+
 from chargeamps.base import (
     ChargePoint,
     ChargePointConnector,
@@ -19,10 +23,6 @@ from chargeamps.base import (
     ChargePointConnectorStatus,
 )
 from chargeamps.external import ChargeAmpsExternalClient
-from homeassistant.const import CONF_API_KEY, CONF_PASSWORD, CONF_URL, CONF_USERNAME
-from homeassistant.helpers import discovery
-from homeassistant.helpers.entity import Entity
-from homeassistant.util import Throttle
 
 from .const import (
     CONF_CHARGEPOINTS,
