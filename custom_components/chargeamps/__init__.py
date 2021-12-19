a1cad5fcc76b4f628236d887d210344ff788c1f7
Jakob Schlyter <jakob@kirei.se>
2021-12-19 16:26:37 +0100

    use extra_state_attributes

--- custom_components/chargeamps/__init__.py
+++ custom_components/chargeamps/__init__.py
@@ -402,7 +402,7 @@ class ChargeampsEntity(Entity):
             return DEFAULT_ICON
 
     @property
-    def device_state_attributes(self):
+    def extra_state_attributes(self):
         """Return the state attributes of the sensor."""
         return self._attributes
 
