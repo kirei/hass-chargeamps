b0ce669f417d09661f60360a69e7cdcfba63067c
Jakob Schlyter <jakob@kirei.se>
2024-09-20 09:16:08 +0200

    remove pydantic

--- custom_components/chargeamps/client.py
+++ custom_components/chargeamps/client.py
@@ -1,45 +1,33 @@
 """Charge-Amps API Client"""
 
-import asyncio  # noqa
-import time
-from urllib.parse import urljoin
 import logging
+import time
+from dataclasses import dataclass
 from datetime import datetime
-
-from pydantic import BaseModel, ConfigDict, PlainSerializer
-from pydantic.alias_generators import to_camel
-from typing_extensions import Annotated
+from urllib.parse import urljoin
 
 import jwt
 from aiohttp import ClientResponse, ClientSession
 from aiohttp.web import HTTPException
+from dataclasses_json import LetterCase, dataclass_json
+
+from .utils import datetime_field
 
 API_BASE_URL = "https://eapi.charge.space"
 API_VERSION = "v5"
 
 
-CustomDateTime = Annotated[
-    datetime,
-    PlainSerializer(lambda _datetime: _datetime.strftime("%Y-%m-%dT%H:%M:%SZ"), return_type=str),
-]
-
-
-class FrozenBaseSchema(BaseModel):
-    model_config = ConfigDict(
-        alias_generator=to_camel,
-        populate_by_name=True,
-        from_attributes=True,
-        frozen=True,
-    )
-
-
-class ChargePointConnector(FrozenBaseSchema):
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass(frozen=True)
+class ChargePointConnector:
     charge_point_id: str
     connector_id: int
     type: str
 
 
-class ChargePoint(FrozenBaseSchema):
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass(frozen=True)
+class ChargePoint:
     id: str
     name: str
     password: str
@@ -50,36 +38,46 @@ class ChargePoint(FrozenBaseSchema):
     connectors: list[ChargePointConnector]
 
 
-class ChargePointMeasurement(FrozenBaseSchema):
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass(frozen=True)
+class ChargePointMeasurement:
     phase: str
     current: float
     voltage: float
 
 
-class ChargePointConnectorStatus(FrozenBaseSchema):
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass(frozen=True)
+class ChargePointConnectorStatus:
     charge_point_id: str
     connector_id: int
     total_consumption_kwh: float
     status: str
-    measurements: list[ChargePointMeasurement] | None = None
-    start_time: CustomDateTime | None = None
-    end_time: CustomDateTime | None = None
-    session_id: int | None = None
+    measurements: list[ChargePointMeasurement] | None
+    start_time: datetime | None = datetime_field()
+    end_time: datetime | None = datetime_field()
+    session_id: str | None = None
 
 
-class ChargePointStatus(FrozenBaseSchema):
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass(frozen=True)
+class ChargePointStatus:
     id: str
     status: str
     connector_statuses: list[ChargePointConnectorStatus]
 
 
-class ChargePointSettings(FrozenBaseSchema):
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass(frozen=False)
+class ChargePointSettings:
     id: str
     dimmer: str
     down_light: bool
 
 
-class ChargePointConnectorSettings(FrozenBaseSchema):
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass(frozen=False)
+class ChargePointConnectorSettings:
     charge_point_id: str
     connector_id: int
     mode: str
@@ -88,17 +86,21 @@ class ChargePointConnectorSettings(FrozenBaseSchema):
     max_current: float | None = None
 
 
-class ChargingSession(FrozenBaseSchema):
-    id: int
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass(frozen=True)
+class ChargingSession:
+    id: str
     charge_point_id: str
     connector_id: int
     session_type: str
     total_consumption_kwh: float
-    start_time: CustomDateTime | None = None
-    end_time: CustomDateTime | None = None
+    start_time: datetime | None = datetime_field()
+    end_time: datetime | None = datetime_field()
 
 
-class StartAuth(FrozenBaseSchema):
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass(frozen=True)
+class StartAuth:
     rfid_length: int
     rfid_format: str
     rfid: str
@@ -190,17 +192,23 @@ class ChargeAmpsClient:
     async def _post(self, path, **kwargs) -> ClientResponse:
         await self._ensure_token()
         headers = kwargs.pop("headers", self._headers)
-        return await self._session.post(urljoin(self._base_url, path), ssl=self._ssl, headers=headers, **kwargs)
+        return await self._session.post(
+            urljoin(self._base_url, path), ssl=self._ssl, headers=headers, **kwargs
+        )
 
     async def _get(self, path, **kwargs) -> ClientResponse:
         await self._ensure_token()
         headers = kwargs.pop("headers", self._headers)
-        return await self._session.get(urljoin(self._base_url, path), ssl=self._ssl, headers=headers, **kwargs)
+        return await self._session.get(
+            urljoin(self._base_url, path), ssl=self._ssl, headers=headers, **kwargs
+        )
 
     async def _put(self, path, **kwargs) -> ClientResponse:
         await self._ensure_token()
         headers = kwargs.pop("headers", self._headers)
-        return await self._session.put(urljoin(self._base_url, path), ssl=self._ssl, headers=headers, **kwargs)
+        return await self._session.put(
+            urljoin(self._base_url, path), ssl=self._ssl, headers=headers, **kwargs
+        )
 
     async def get_chargepoints(self) -> list[ChargePoint]:
         """Get all owned chargepoints"""
@@ -208,7 +216,7 @@ class ChargeAmpsClient:
         response = await self._get(request_uri)
         res = []
         for chargepoint in await response.json():
-            res.append(ChargePoint.model_validate(chargepoint))
+            res.append(ChargePoint.from_dict(chargepoint))
         return res
 
     async def get_all_chargingsessions(
@@ -223,59 +231,71 @@ class ChargeAmpsClient:
             query_params["startTime"] = start_time.isoformat()
         if end_time:
             query_params["endTime"] = end_time.isoformat()
-        request_uri = f"/api/{API_VERSION}/chargepoints/{charge_point_id}/chargingsessions"
+        request_uri = (
+            f"/api/{API_VERSION}/chargepoints/{charge_point_id}/chargingsessions"
+        )
         response = await self._get(request_uri, params=query_params)
         res = []
         for session in await response.json():
-            res.append(ChargingSession.model_validate(session))
+            res.append(ChargingSession.from_dict(session))
         return res
 
-    async def get_chargingsession(self, charge_point_id: str, session: int) -> ChargingSession:
+    async def get_chargingsession(
+        self, charge_point_id: str, session: int
+    ) -> ChargingSession:
         """Get charging session"""
         request_uri = f"/api/{API_VERSION}/chargepoints/{charge_point_id}/chargingsessions/{session}"
         response = await self._get(request_uri)
         payload = await response.json()
-        return ChargingSession.model_validate(payload)
+        return ChargingSession.from_dict(payload)
 
     async def get_chargepoint_status(self, charge_point_id: str) -> ChargePointStatus:
         """Get charge point status"""
         request_uri = f"/api/{API_VERSION}/chargepoints/{charge_point_id}/status"
         response = await self._get(request_uri)
         payload = await response.json()
-        return ChargePointStatus.model_validate(payload)
+        return ChargePointStatus.from_dict(payload)
 
-    async def get_chargepoint_settings(self, charge_point_id: str) -> ChargePointSettings:
+    async def get_chargepoint_settings(
+        self, charge_point_id: str
+    ) -> ChargePointSettings:
         """Get chargepoint settings"""
         request_uri = f"/api/{API_VERSION}/chargepoints/{charge_point_id}/settings"
         response = await self._get(request_uri)
         payload = await response.json()
-        return ChargePointSettings.model_validate(payload)
+        return ChargePointSettings.from_dict(payload)
 
     async def set_chargepoint_settings(self, settings: ChargePointSettings) -> None:
         """Set chargepoint settings"""
-        payload = settings.model_dump(by_alias=True)
+        payload = settings.to_dict()
         charge_point_id = settings.id
         request_uri = f"/api/{API_VERSION}/chargepoints/{charge_point_id}/settings"
         await self._put(request_uri, json=payload)
 
-    async def get_chargepoint_connector_settings(self, charge_point_id: str, connector_id: int) -> ChargePointConnectorSettings:
+    async def get_chargepoint_connector_settings(
+        self, charge_point_id: str, connector_id: int
+    ) -> ChargePointConnectorSettings:
         """Get all owned chargepoints"""
         request_uri = f"/api/{API_VERSION}/chargepoints/{charge_point_id}/connectors/{connector_id}/settings"
         response = await self._get(request_uri)
         payload = await response.json()
-        return ChargePointConnectorSettings.model_validate(payload)
+        return ChargePointConnectorSettings.from_dict(payload)
 
-    async def set_chargepoint_connector_settings(self, settings: ChargePointConnectorSettings) -> None:
+    async def set_chargepoint_connector_settings(
+        self, settings: ChargePointConnectorSettings
+    ) -> None:
         """Get all owned chargepoints"""
-        payload = settings.model_dump(by_alias=True)
+        payload = settings.to_dict()
         charge_point_id = settings.charge_point_id
         connector_id = settings.connector_id
         request_uri = f"/api/{API_VERSION}/chargepoints/{charge_point_id}/connectors/{connector_id}/settings"
         await self._put(request_uri, json=payload)
 
-    async def remote_start(self, charge_point_id: str, connector_id: int, start_auth: StartAuth) -> None:
+    async def remote_start(
+        self, charge_point_id: str, connector_id: int, start_auth: StartAuth
+    ) -> None:
         """Remote start chargepoint"""
-        payload = start_auth.model_dump(by_alias=True)
+        payload = start_auth.to_dict()
         request_uri = f"/api/{API_VERSION}/chargepoints/{charge_point_id}/connectors/{connector_id}/remotestart"
         await self._put(request_uri, json=payload)
 
